7ec05df8b8a68659d063bf8b227a2325d8cd50b4
freezeava <aashishava301@gmail.com>
2024-03-10 11:38:39 +0530

    learning Function-

--- functions/SwapFun.java
+++ functions/SwapFun.java
@@ -10,6 +10,8 @@ public class SwapFun {
         b=temp;
         System.out.println("After swappig: a= "+a+" and b= "+b);
     }
+  
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
