6d16aea5c781a0f991b8006d477d759553a53060
freezeava <aashishava301@gmail.com>
2024-03-12 08:27:13 +0530

    Learning Array and strings

---
+++ StringsInjava/StringCocn.java
@@ -0,0 +1,19 @@
+public class StringCocn {
+
+    public static void main(String[] args) {
+        
+        char ch[]={'a','b','c','d','e'};
+
+        for(int i=0;i<ch.length;i++){
+            System.out.print(ch[i]+" ");
+        }
+        System.out.println();
+
+
+        String str="My name is Tony Stark";
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+"      ");
+        }
+    }
+    
+}
